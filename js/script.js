e9cc6bca580b993f34ac3683171ce32fd13ab1f3
EricMB83 <ermabe.design@gmail.com>
2020-08-02 11:14:49 +0200

    adding loop active

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ window.onload = function(){
     var selectorFecha = fecha.getFullYear();
     document.getElementById("fecha_copy").innerHTML = selectorFecha;
 }
+
+function cambiarApartado(numero){
+    var contenedorArray = document.getElementsByClassName('menu_superior');
+
+    for(i = 0; i < contenedorArray.length; i++){
+        var contenedorNumero = document.getElementById("seccion_muestra_" + i);
+        contenedorNumero.classList.remove('active');
+        contenedorArray[i].classList.remove('active');
+    }
+
+    document.getElementById("seccion_muestra_" + numero).classList.toggle('active');
+    contenedorArray[numero].classList.toggle('active');
+}

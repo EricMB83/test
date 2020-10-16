78306469d94503143ac0ba5422b6a1de4c0e6d35
Eric <Eric@iMac.local>
2020-10-16 09:57:50 +0200

    solved filter issue

--- js/script.js
+++ js/script.js
@@ -18,18 +18,21 @@ function cambiarApartado(numero){
 }
 
 /* FILTRO */
-/*
-var contenedor = document.getElementsByClassName("imagenes-pb");
-let result = new Array();
 
-for(let i=0; i<contenedor.length; i++) result.push(1);
+var contenedor = document.getElementsByClassName("imagenes-pb");
+let result = new Array(5);
+let i = 0;
+
+do{
+    result[i] = 1;
+    console.log(result);
+    i++;
+} while(i < 5);
 
 function filtroTriple(evt, grupo, filtro){
     if(evt != undefined || evt != null) evt.preventDefault();
     var selectorLista = document.getElementsByClassName(grupo);
-
-    console.log("primera: " + result);
-
+    console.log("contenedor: " + contenedor.length);
     for(let i=0; i<contenedor.length; i++) contenedor[i].classList.add("active");
 
     switch(grupo){
@@ -51,40 +54,31 @@ function filtroTriple(evt, grupo, filtro){
             }
             selectorLista[0].children[filtro].children[0].classList.add("selected");
             break;
-        case "grp3":
-            for(i = 0; i < selectorLista[0].children.length; i++){
-                selectorLista[0].children[i].children[0].classList.remove("selected");
-            }
-            selectorLista[0].children[filtro].children[0].classList.add("selected");
-            break;
-        case "grp4":
-            for(i = 0; i < selectorLista[0].children.length; i++){
-                selectorLista[0].children[i].children[0].classList.remove("selected");
-            }
-            selectorLista[0].children[filtro].children[0].classList.add("selected");
-            break;
     }
 
     var clase0 = document.getElementsByClassName('grp0')[0].querySelector('.selected').name;
     var clase1 = document.getElementsByClassName('grp1')[0].querySelector('.selected').name;
     var clase2 = document.getElementsByClassName('grp2')[0].querySelector('.selected').name;
-    var clase3 = document.getElementsByClassName('grp3')[0].querySelector('.selected').name;
-    var clase4 = document.getElementsByClassName('grp4')[0].querySelector('.selected').name;
-
-    filtrarElementos(clase0);
-    filtrarElementos(clase1);
-    filtrarElementos(clase2);
-    filtrarElementos(clase3);
-    filtrarElementos(clase4);
+    
+    filtrarElementos(clase0, clase1, clase2);
+    console.log(result);
 
     for(let i=0; i<contenedor.length; i++) if(result[i] === 0) contenedor[i].classList.remove("active");
-
-    console.log("segunda: " + result);
 }
 
-function filtrarElementos(clase){
+function filtrarElementos(cls0, cls1, cls2){
     for(let i=0; i<contenedor.length; i++)
     {
-        if(contenedor[i].classList.contains(clase) == false) result[i] = 0;
+        if(contenedor[i].classList.contains(cls0) == false || contenedor[i].classList.contains(cls1) == false || contenedor[i].classList.contains(cls2) == false){
+            result[i] = 0;
+        }else{
+            result[i] = 1;
+        }
     }
-}*/
+}
+
+/*
+
+HACES UN FILTRO QUE COMPARE
+
+*/
